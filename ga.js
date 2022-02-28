1f22c8e3362f3987055e37e0e07920718b6b3366
rdiaz <rodrigo.diaz@frba.utn.edu.ar>
2022-02-28 01:45:23 -0300

    Added stats

--- ga.js
+++ ga.js
@@ -5,7 +5,7 @@ class GeneticAlgorithm {
   }
 
   setup () {
-    this.exactWord = 'Rodri capo'
+    this.exactWord = 'Rodri capo 1'
     this.mutationProbability = 0.01
     this.populationSize = 200
     this.maxGenerations = 1000
@@ -59,7 +59,7 @@ class GeneticAlgorithm {
     // Reproduction
     for (let i = 0; i < this.populationSize ; i++) {
       const parentA = this.matingPool[Math.floor(Math.random() * this.matingPool.length)]
-      const parentB = this.matingPool[Math.floor(Math.random() * this.matingPool.length)]
+      const parentB = this.getDifferentParent(parentA)
       const child = parentA.crossover(parentB)
       newPopulation.push(child)
     }
@@ -71,11 +71,20 @@ class GeneticAlgorithm {
     return newPopulation
   }
 
+  getDifferentParent (parent) {
+    let posibleParent = null
+    do {
+      posibleParent = this.matingPool[Math.floor(Math.random() * this.matingPool.length)]
+    } while (posibleParent === parent)
+    return posibleParent
+  }
+
   generateStats () {
     console.log('----------------------------------------')	
     console.log('Generation: ' + this.generation)
     console.log('Best: ' + this.best.toString())
     console.log('Fitness: ' + this.best.fitness)
+    console.log('Average fitness: ' + this.population.reduce((prev, curr) => prev + curr.fitness, 0) / this.population.length)
     console.log('----------------------------------------')
   }
 }
